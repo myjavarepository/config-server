6e3f8c0779dd1cff7b90cf683a727470cb929634
Pankaj K Singh <pankajpankaj2006@gmail.com>
2023-03-19 10:38:08 +0530

    Config server
    config server

---
+++ configserver/src/test/java/com/eschool/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eschool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
